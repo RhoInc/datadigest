26fd4bc0e35b8099b29b645b156da5cda7271e99
Jeremy Wildfire <jwildfire@gmail.com>
2017-08-12 09:36:07 -0700

    add file upload button using shiny server.R

--- R/explorerApp.R
+++ R/explorerApp.R
@@ -8,25 +8,27 @@
 #' @export
 
 explorerApp <- function(){
-  ui = miniPage(
-    miniContentPanel(
-      explorerOutput("exp_int")
-    ),
+  ui = basicPage(
+    explorerOutput("exp_int"),
     suppressDependencies("bootstrap")
-  )
+    )
 
   server = function(input, output, session){
+    
+    #Draw the explorer
     output$exp_int <- renderCodebook({
       explorer(demo=T)
     })
-    observeEvent(input$cancel, {
-      stopApp()
-    })
-    observeEvent(input$done, {
-      stopApp()
-    })
+
+    #wait a second and add a file button
+    Sys.sleep(1)
+    insertUI(
+      selector="div.explorer div.instructions.section",
+      where="beforeEnd",
+      ui=fileInput('datafile','Upload a file',accept = c('.sas7bdat','.csv'))
+    )
   }
 
-  runGadget(ui, server, viewer = dialogViewer("Codebook add-in", width=1100, height=1000))
-  #runGadget(ui, server, viewer = browserViewer(browser = getOption("browser")))
+  #runGadget(ui, server, viewer = dialogViewer("Codebook add-in", width=1100, height=1000))
+  runGadget(ui, server, viewer = browserViewer(browser = getOption("browser")))
 }
