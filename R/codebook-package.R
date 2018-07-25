4fc054f0d0dd883f7450dd34f196e88b752d7bb1
bzkrouse <rebecca_zabel@rhoworld.com>
2018-07-25 09:30:37 -0400

    begin package help page

---
+++ R/codebook-package.R
@@ -0,0 +1,15 @@
+#' codebook
+#'
+#' The **codebook** package provides a simple interactive framework for exploring data.  
+#' The package serves as an R interface for Rho's [web-codebook](https://github.com/RhoInc/web-codebook) 
+#' JavaScript library. This tool provides a concise summary of every variable in a data frame and 
+#' includes interactive features such as real-time filters, grouping, and highlighting. 
+#' The R interface allows the analyst to interactively explore datasets within the typical 
+#' analysis workflow and working environment.  
+#' **codebook** has been built using the [htmlwidgets](htmlwidgets.org) framework.
+#'
+#' @source Rho's web-codebook JavaScript library: \url{https://github.com/RhoInc/web-codebook}.
+#'
+#' @docType package
+#' @name codebook 
+#' @keywords internal 
